b64964ce0c3cb64840e2722d4482793ed4a214e0
prystaia <prystaia@gmail.com>
2023-09-10 12:58:25 +0300

    Evenly distributed

--- app/src/main/java/com/example/compose_1/MainActivity.kt
+++ app/src/main/java/com/example/compose_1/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.compose_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose_1.ui.theme.Compose_1Theme
 
@@ -24,7 +32,34 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun NewText(){
-    Text(text = "Hello 4!",)
+    Row (
+        modifier = Modifier
+            .background(Color.Gray).fillMaxWidth().fillMaxHeight(0.5f)
+//        horizontalArrangement = Arrangement.Center,
+//        verticalAlignment = Alignment.Top
+    ){
+        Column (
+            modifier = Modifier
+                .background(Color.Green).fillMaxWidth(0.5f).fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Hello!")
+            Text(text = "Everyone!")
+            Text(text = "Last element!")
+        }
+        Column (
+            modifier = Modifier
+                .background(Color.Red).fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Hello!")
+            Text(text = "Everyone!")
+            Text(text = "Last element!")
+        }
+    }
+
 }
 
 
