bacf326b6dea9126836939c56c360c2e1472dac7
prystaia <prystaia@gmail.com>
2023-09-10 13:00:04 +0300

    Optimized

--- app/src/main/java/com/example/compose_1/MainActivity.kt
+++ app/src/main/java/com/example/compose_1/MainActivity.kt
@@ -10,15 +10,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose_1.ui.theme.Compose_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
